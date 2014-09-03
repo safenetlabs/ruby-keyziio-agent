320f2dd9ec91abc7d133f3de4fa8a80e2f63aee5
David Pitard <david.pitard@safenet-inc.com>
2014-09-03 14:36:09 -0400

    Keychain methods work.  Added exception handling.

--- lib/keyziio/agent.rb
+++ lib/keyziio/agent.rb
@@ -2,12 +2,18 @@ require 'rest-client'
 require 'json'
 
 module Keyziio
+  class Unauthorized < StandardError
+  end
+
+  class ServerError < StandardError
+  end
+
+  class ResourceNotFound < StandardError
+  end
+
   class Agent
 
-    # Note: not passing through standard exceptions - an enhancement would be to return
-    # meaningful exceptions for expected failure cases (bad auth, no connection, etc.)
-
-    attr_accessor :id, :secret, :base_url, :token_hash, :token_end_time
+    attr_accessor :id, :secret, :base_url, :token_hash
 
     def check_or_get_token
       if !(self.token_hash).nil?
@@ -17,54 +23,66 @@ module Keyziio
       uri = URI.parse("#{base_url}/oauth2/token")
       uri.user = id
       uri.password = secret
-      begin
-        response = RestClient.post(uri.to_s , {:grant_type => "client_credentials"}.to_json, {:content_type => :json, :accept => :json})
-        self.token_hash = JSON.parse(response)
-      rescue SocketError, RestClient::Unauthorized
-        false
-      end
+      response = RestClient.post(uri.to_s , {:grant_type => "client_credentials"}.to_json, {:content_type => :json, :accept => :json})
+      self.token_hash = JSON.parse(response)
     end
 
     def initialize(id, secret, base_url = 'https://keyziio2.herokuapp.com/api/v1')
       self.base_url = base_url
       self.id = id
       self.secret = secret
-      self.check_or_get_token # Not really necessary here, but why not get it early
+      begin
+        self.check_or_get_token # Not really necessary here, but why not get it early.  Ignore expected exceptions
+      rescue SocketError, RestClient::Unauthorized
+        true
+      end
     end
 
     def check
-      if !self.check_or_get_token
-        return false
-      end
-
       begin
+        self.check_or_get_token
         url = "#{base_url}/oauth2/token/info"
         RestClient.get url, {:accept => :json, :authorization => "Bearer #{token_hash['access_token']}"}
-      rescue SocketError, RestClient::Unauthorized
-        false
-      end
-    end
-
-    def get_user (id)
-      if !self.check_or_get_token
-        return false
-      end
-
-      begin
-        url = "#{base_url}/users/#{id}?api_token=#{api_token}"
-        RestClient.get url, {:accept => :json, :authorization => "Bearer #{token_hash['access_token']}"}
-      rescue SocketError, RestClient::Unauthorized
-        false
-      end
-    end
-
-    def create_keychain (id, friendly_name)
-      begin
-        url = "#{base_url}/api/v1/agent/keychains"
-        RestClient.post url, {:accept => :json, :authorization => "Bearer #{token_hash['access_token']}"}
       rescue RestClient::Unauthorized
-        false
+        raise Unauthorized
+      rescue SocketError => e
+        raise ServerError, e.message
       end
+    end
+
+    def get_keypart
+      url = "#{base_url}/hsm/keyparts"
+      response = RestClient.post url,{}, {:accept => :json, :authorization => "Bearer #{token_hash['access_token']}"}
+      JSON.parse(response)["bytes"]
+    end
+
+    def create_keychain (name)
+      begin
+        self.check_or_get_token
+        keypart = self.get_keypart
+        url = "#{base_url}/agent/keychains"
+        response = RestClient.post url,{:name => name, :keypart => keypart}, {:accept => :json, :authorization => "Bearer #{token_hash['access_token']}"}
+        JSON.parse(response)["id"]
+      rescue RestClient::Unauthorized
+        raise Unauthorized
+      rescue SocketError => e
+        raise ServerError, e.message
+      end
+    end
+
+    def get_keychain (id)
+      begin
+        self.check_or_get_token
+        url = "#{base_url}/agent/keychains/#{id}"
+        response = RestClient.get url, {:accept => :json, :authorization => "Bearer #{token_hash['access_token']}"}
+      rescue RestClient::Unauthorized
+        raise Unauthorized
+      rescue SocketError => e
+        raise ServerError, e.message
+      rescue RestClient::ResourceNotFound => e
+        raise ResourceNotFound, e.message
+      end
+      
     end
   end
 end
--- test/test_keyziio.rb
+++ test/test_keyziio.rb
@@ -19,56 +19,36 @@ class TestKeyziio < MiniTest::Test
 
   def test_check_bad_creds
     agent = Keyziio::Agent.new('b4393ce46503757f5753281b1e0ab64d9d10fd2652e4eee683fd17054590e605', 'badpassword')
-    refute agent.check
+    assert_raises Keyziio::Unauthorized do
+      agent.check
+    end
   end
 
   def test_check_no_connection
-    agent = Keyziio::Agent.new('b4393ce46503757f5753281b1e0ab64d9d10fd2652e4eee683fd17054590e605', '919c9236eb16d9b52b06eed69c27b5cbbdf6a23e9c27c3cf221684906f83948d','https://www.notarealsiteihope.com')
-    refute agent.check
+    assert_raises Keyziio::ServerError do
+      agent = Keyziio::Agent.new('b4393ce46503757f5753281b1e0ab64d9d10fd2652e4eee683fd17054590e605', '919c9236eb16d9b52b06eed69c27b5cbbdf6a23e9c27c3cf221684906f83948d','https://www.notarealsiteihope.com')
+      refute agent.check
+    end
   end
 
   def test_create_keychain
     agent = Keyziio::Agent.new('b4393ce46503757f5753281b1e0ab64d9d10fd2652e4eee683fd17054590e605', '919c9236eb16d9b52b06eed69c27b5cbbdf6a23e9c27c3cf221684906f83948d')
     assert agent.check
+    assert agent.create_keychain("name")
+  end
 
+  def test_get_keychain
+    agent = Keyziio::Agent.new('b4393ce46503757f5753281b1e0ab64d9d10fd2652e4eee683fd17054590e605', '919c9236eb16d9b52b06eed69c27b5cbbdf6a23e9c27c3cf221684906f83948d')
+    assert agent.check
+    id = agent.create_keychain("name")
+    assert agent.get_keychain(id)
   end
 
   def test_get_a_keychain_which_isnt_there
-
+    agent = Keyziio::Agent.new('b4393ce46503757f5753281b1e0ab64d9d10fd2652e4eee683fd17054590e605', '919c9236eb16d9b52b06eed69c27b5cbbdf6a23e9c27c3cf221684906f83948d')
+    assert_raises Keyziio::ResourceNotFound do
+      agent.get_keychain("72188084-fb5f-445c-901e-05afcee24a9f")
+    end
   end
-
-
-  #
-  # def test_create_user
-  #
-  # end
-
-  #
-  # def test_failed_check
-  #   VCR.use_cassette('failed_test_check') do
-  #     agent = Keyziio::Agent.new('invalid')
-  #     refute agent.check
-  #   end
-  # end
-
-  # def test_check_no_connection
-  #   VCR.use_cassette('test_check_no_connection') do
-  #     agent = Keyziio::Agent.new('yrDIo1JeXlR74FGpyHyi') #, 'http://keyziio.herokuapp.com/willnotbefound')
-  #     assert_raises RestClient::ResourceNotFound do
-  #       agent.check
-  #     end
-  #   end
-  # end
-
-  # def test_get_user
-  #   agent = Keyziio::Agent.new('cnZNqHoOXDML9eSSZI')
-  #   agent.get_user('billy')
-  # end
-  #
-  # def test_create_user
-  #   agent = Keyziio::Agent.new('cnZNqHoOXDML9eSSZI')
-  #   p = SecureRandom.hex
-  #   agent.create_user(p, 'friendly')
-  # end
 
 end
